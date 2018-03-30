3e74cd04d47a50f0ee88f97478c89e3dcd946f95
Yu-Jin Tan <yujinost@gmail.com>
2018-03-30 23:20:51 +0100

    setting up card project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'; //this is only needed in App.js
-import Projects from './Components/Projects'
+import Cards from './Components/Cards'
 
 class App extends Component {
+  constructor(){
+    super();
+    //we want to take this state and pass through <cards /> 
+    //as a property
+    this.state = {
+      cards: [
+        {
+          name:'K'
+        },
+        {
+          name:'Q'
+        },
+        {
+          name:'J'
+        },
+        {
+          name:'10'
+        },
+        {
+          name:'9'
+        },
+        {
+          name:'8'
+        },
+        {
+          name:'7'
+        },
+        {
+          name:'6'
+        },
+        {
+          name:'5'
+        },
+        {
+          name:'4'
+        },
+        {
+          name:'3'
+        },
+        {
+          name:'2'
+        },
+        {
+          name:'A'
+        }
+      ]
+    }
+  }
   render() {
     return (
       // can only have one div at the very top level
       <div className="App"> 
         My app
-        <Projects test = " Hello World "/>
-        
+        <Cards name = " Hello World "/>      
       </div>
     );
   }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
 //follows the structure of App.js
-class Projects extends Component {
+class Cards extends Component {
   render() {
     return (
-      <div className="Projects"> 
-            My Projects
-            {this.props.test}
+      <div className="Cards"> 
+            My Cards
+            {this.props.name}
       </div>
     );
   }
 }
 
 //So we can use it in other files
-export default Projects;
+export default Cards;
